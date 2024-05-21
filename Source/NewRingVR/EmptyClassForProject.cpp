4a99f4ac08d3fac317b47b33622249396b25d174
Dmitry Grishin <grishinda10@ya.ru>
2024-05-22 03:35:51 +0500

    init

---
+++ Source/NewRingVR/EmptyClassForProject.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+//#include "NewRingVR.h"
+#include "EmptyClassForProject.h"
+
+EmptyClassForProject::EmptyClassForProject()
+{
+}
+
+EmptyClassForProject::~EmptyClassForProject()
+{
+}
